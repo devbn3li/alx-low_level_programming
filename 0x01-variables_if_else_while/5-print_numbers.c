e4dc84ec2f2fa98fb1eecec176e6e18ea33aaa16
Bn3li <fudwhc@gmail.com>
2023-03-17 07:34:40 -0700

    add file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
