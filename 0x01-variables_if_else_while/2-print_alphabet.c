e2d6934198ae441d5da6c6b04e41151b3f097600
Bn3li <fudwhc@gmail.com>
2023-03-17 07:22:56 -0700

    add file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
