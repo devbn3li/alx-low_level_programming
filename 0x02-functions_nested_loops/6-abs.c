99848da8eb861239d2d866472ded2cd5e3d11389
Bn3li <fudwhc@gmail.com>
2023-03-21 14:49:55 -0700

    first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Entry Point
+ * @num: integer
+ * Return: 0 (Success)
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
