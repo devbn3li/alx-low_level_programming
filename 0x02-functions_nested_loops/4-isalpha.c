04660020752bb7ac43427644c19d158bf1ed98b1
Bn3li <fudwhc@gmail.com>
2023-03-21 14:46:44 -0700

    first commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Entry Point
+ * @c: - char
+ * Return: 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
