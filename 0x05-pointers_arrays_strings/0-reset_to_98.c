6e60fa65cd49ff1e9e46dc868e4940c0fce2d8c6
Bn3li <fudwhc@gmail.com>
2023-03-27 15:50:33 -0700

    first commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *0-reset_to_98.c - function that takes a pointer to an int as parameter
+ *0-reset_to_98 - function that takes a pointer to an int as parameter
  *make new value 98
  *@n: int
  */
