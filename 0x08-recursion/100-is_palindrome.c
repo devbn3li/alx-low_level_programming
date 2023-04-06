71133fd3ba24d126ff364765a2beeb0aa2196103
Bn3li <fudwhc@gmail.com>
2023-04-05 19:34:47 -0700

    new file

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,46 +1,40 @@
 #include "main.h"
-
 /**
- * is_palindrome - checks if a given string is a palindrome.
- * @s: string to check.
- *
- * Return: 1 if a string is a palindrome and 0 if not.
- */
+   * palindromeChecker - checks the string
+    * @str: the string to be checked
+     * @len: the length of the string
+      * @i: the incrementor, starts at 0
+       * Return: 1 if it's a palindrome, 0 if it's not
+	*/
+int palindromeChecker(char *str, int len, int i)
+{
+		if (i < len && str[i] == str[len])
+			return (palindromeChecker(str, len - 1, i + 1));
+		if (str[i] != str[len])
+			return (0);
+		return (1);
+}
+/**
+   * _strlen_recursion - returns the length of a string
+    * @s: the string to check the length of
+     * Return: an integer that displays the length of the string
+      */
+int _strlen_recursion(char *s)
+{
+		if (*s != '\0')
+			return (1 + _strlen_recursion(s + 1));
+
+		return (0);
+}
+/**
+   * is_palindrome - checks to see if a string is a palindrome
+    * @s: the string to check
+     * Return: 1 if it's a palindrome, 2 if it's not
+      */
 int is_palindrome(char *s)
 {
-	return (is_palindrome_helper(s, 0, is_palindrome_strlen(s) - 1));
-}
+		int i = 0;
+			int length = _strlen_recursion(s) - 1;
 
-/**
- * is_palindrome_strlen - returns the length of a string.
- * @s: string to check.
- *
- * Return: length of the string
- */
-int is_palindrome_strlen(char *s)
-{
-	int len = 0;
-
-	if (*s != '\0')
-		len += is_palindrome_strlen(s + 1) + 1;
-
-	return (len);
-}
-
-/**
- * is_palindrome_helper - checks if a given substring is a palindrom.
- * @s: string to check.
- * @left: index of the leftmost character of the substring.
- * @right: index of the rightmost character of the substring.
- *
- * Return: 1 if the substring is a palindrome and 0 if not.
- */
-int is_palindrome_helper(char *s, int left, int right)
-{
-	if (left >= right)
-		return (1);
-	else if (s[left] == s[right])
-		return (is_palindrome_helper(s, left + 1, right - 1));
-	else
-		return (0);
+				return (palindromeChecker(s, length, i));
 }
