735fafd40c73800052bfc8361ff98ead9bc7e11b
Bn3li <fudwhc@gmail.com>
2023-04-17 16:53:47 -0700

    add file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program compilation starts from the main
+ * Description: program prints the name of the file it was comipiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
+}
