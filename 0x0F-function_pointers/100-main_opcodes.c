2de8452bc97f7723635fed2a69e4677b4038d8ee
Bn3li <fudwhc@gmail.com>
2023-04-19 19:31:19 -0700

    add file

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,38 +2,38 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
+ * main - check the code.
+ * @argc: argument count
+ * @argv: argument vector
  *
- * Return: Always 0.
+ * Return: 0 true
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	char *opc = (char *) main;
+	int i, nbytes;
 
-    int num_bytes = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    if (num_bytes <= 0)
-    {
-        printf("Error\n");
-        return 2;
-    }
+	nbytes = atoi(argv[1]);
 
-    unsigned char *p = (unsigned char *)&main;
-    int i;
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x ", *p);
-        p++;
-    }
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
 
-    printf("\n");
-
-    return 0;
+	printf("\n");
+	return (0);
 }
