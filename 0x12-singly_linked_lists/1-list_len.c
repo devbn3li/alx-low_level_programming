64ff0f70bcb1ed4804677817213f5d29dbfb3cfc
devbn3li <fudwhc@gmail.com>
2023-04-27 12:28:16 -0700

    add file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * list_len - the number of nodes
+ *
+ * @h: pointer to the head
+ *
+ * Return: nodes number
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 1;
+
+	if (h == NULL)
+		return (0);
+
+	/*iterate through the nodes*/
+	while (h->next != NULL)
+	{
+		/*go to the next node*/
+		h = h->next;
+		/*count node*/
+		node_count++;
+	}
+
+	return (node_count);
+}
