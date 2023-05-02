231ae2544d95cf8ba88bee51895ca66d58df30b3
Bn 3li <fudwhc@gmail.com>
2023-05-02 16:32:53 +0300

    fixes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,6 +8,13 @@
 size_t print_listint(const listint_t *h)
 {
     size_t count = 0;
+    const listint_t *current = h;
 
+    while (current)
+    {
+        printf("%d\n", current->n);
+        count++;
+        current = current->next;
+    }
     return (count);
 }
