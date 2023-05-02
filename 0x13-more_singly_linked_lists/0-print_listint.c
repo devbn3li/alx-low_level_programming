e14c90b1629f58f0a1b3cc5de606c21af0969302
Bn 3li <fudwhc@gmail.com>
2023-05-02 18:00:25 +0300

    fix betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
-    const listint_t *current = h;
+	size_t count = 0;
+	const listint_t *current = h;
 
-    while (current)
-    {
-        printf("%d\n", current->n);
-        count++;
-        current = current->next;
-    }
-    return (count);
+	while (current)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return (count);
 }
