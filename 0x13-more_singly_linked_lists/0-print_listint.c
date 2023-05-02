9c3dd55f3a899d94599d0d23113ef3c20527d415
Bn 3li <fudwhc@gmail.com>
2023-05-02 16:26:17 +0300

    fixes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,5 @@ size_t print_listint(const listint_t *h)
 {
     size_t count = 0;
 
-    if (h == NULL)
-        return (0);
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-
-        count++;
-        h = h->next;
-    }
-
     return (count);
 }
