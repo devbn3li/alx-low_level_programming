234e5807027c463a6c005fa20e3c847304b45dfd
Bn 3li <fudwhc@gmail.com>
2023-05-12 19:17:46 +0300

    fix main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
