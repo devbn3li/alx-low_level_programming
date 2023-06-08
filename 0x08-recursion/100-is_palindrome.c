0c8f8d802817a27feff1181ca8b37de25f6b25f7
devbn3li <fudwhc@gmail.com>
2023-06-09 00:07:18 +0300

    add files

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * is_palindrome - main
+ * @s: argu
+ */
+int is_palindrome(char *s)
+{
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * wildcmp - main
+ * @s1: arg
+ * @s2: arg
+ */
+int wildcmp(char *s1, char *s2)
+{
+}
