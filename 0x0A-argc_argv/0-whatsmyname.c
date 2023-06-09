ce5e40e684a8d8968ea660d4b4b39e6a57a99338
devbn3li <fudwhc@gmail.com>
2023-06-09 21:42:40 +0300

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: arg count
+ * @argv: arg vector
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
