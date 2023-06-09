83bdb274d4122279d227aa808c07e78e5d203461
devbn3li <fudwhc@gmail.com>
2023-06-09 22:13:24 +0300

    add file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints all arguments it receives
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: always 0
+ */
+int main(int argc, char argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
