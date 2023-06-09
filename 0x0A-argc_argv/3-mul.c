16ff06f7dbfceef5ef7f18ab3cb65a34332c2645
devbn3li <fudwhc@gmail.com>
2023-06-09 23:15:01 +0300

    fix betty

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,19 +7,19 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1;
-    int num2;
+	int num1;
+	int num2;
 
-    if (argc != 3)
-    {
-        printf("Erorr\n");
-        return (1);
-    }
+	if (argc != 3)
+	{
+		printf("Erorr\n");
+		return (1);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-    printf("%d\n", num1 * num2);
+	printf("%d\n", num1 * num2);
 
-    return (0);
+	return (0);
 }
