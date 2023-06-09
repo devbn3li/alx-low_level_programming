c92300f2cf24b65e2a54abf432f11b838b09dea6
devbn3li <fudwhc@gmail.com>
2023-06-09 10:40:36 +0300

    new files

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks if input is lowercase
+ * @c: variable to determine if input is lowercase
+ * Return: 0 if succesful
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+else
+	return (0);
+}
