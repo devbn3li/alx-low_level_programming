23283ac57c98cd56273a9b9c8d2d6bb5d9a1e96e
devbn3li <fudwhc@gmail.com>
2023-06-09 21:51:49 +0300

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of arguments passed into it
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+    printf("%d\n", argc);
+    return (0);
+}
