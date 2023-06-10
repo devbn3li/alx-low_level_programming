ab4b8017f129c2f489f38b0a6406baf1227cc6b6
devbn3li <fudwhc@gmail.com>
2023-06-10 03:56:53 +0300

    fix betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j, result = 0;
+	int i;
+	int j;
+	int result = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        result += atoi(argv[i]);
-    }
-    printf("%d\n", result);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
