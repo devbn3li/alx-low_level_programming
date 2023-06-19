f2dbe61d2a96c0dbcfa8fca33223ac535dd83657
devbn3li <fudwhc@gmail.com>
2023-06-19 16:58:26 +0300

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Poppy
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
