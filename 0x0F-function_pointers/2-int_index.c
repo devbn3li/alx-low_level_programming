071c4e909c869e2019848d5940a7de0ba708f85d
devbn3li <fudwhc@gmail.com>
2023-06-22 02:02:59 +0300

    d

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (!array || !cmp)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
 
 	return (-1);
