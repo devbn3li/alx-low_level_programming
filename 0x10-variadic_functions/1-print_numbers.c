5f9e5aa6708cfe5503197d94ad390643e4679fd7
devbn3li <fudwhc@gmail.com>
2023-06-22 19:30:56 +0300

    f

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - function print numbers
+ * @separator: comma
+ * @int: number count
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	int i;
+
+	va_start(valist, n);
+	if (separator == NULL)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", va_arg(valist, int), separator);
+	}
+	printf("\n");
+}
