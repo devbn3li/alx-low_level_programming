0ce816de761608fcd8909c208174d6ac88c0a3d5
devbn3li <fudwhc@gmail.com>
2023-06-22 20:25:57 +0300

    hgfd

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,7 +5,6 @@
  * print_numbers - function print numbers
  * @separator: comma
  * @n: number count
- * @...: Arg
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -15,7 +14,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(valist, n);
 	if (separator == NULL)
+	{
 		return;
+	}
 
 	for (i = 0; i < n; i++)
 	{
