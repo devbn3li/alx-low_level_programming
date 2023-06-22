61c9a0c190067b6ac0a582b861b0222e894d803b
devbn3li <fudwhc@gmail.com>
2023-06-22 20:36:28 +0300

    try this

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(valist, n);
-	if (separator == NULL)
-	{
-		return;
-	}
 
 	for (i = 0; i < n; i++)
 	{
