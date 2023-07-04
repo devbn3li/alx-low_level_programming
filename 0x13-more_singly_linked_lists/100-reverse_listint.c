e1c8f5f0b7a4733679032f7f29a9cd09514d8228
devbn3li <fudwhc@gmail.com>
2023-07-04 17:12:21 +0300

    end

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses linked list
+ * @head: pointer to first node in list
+ * Return: pointer to first node in new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * print_listint_safe - func that print linked list with loop safely
+ * @head: pointer to first node of linked list
+ * Return: new_node.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tmp_n = NULL;
+	const listint_t *l_n = NULL;
+	size_t counter = 0;
+	size_t new_n;
+
+	tmp_n = head;
+	while (tmp_n)
+	{
+		printf("[%p] %d\n", (void *)tmp_n, tmp_n->n);
+		counter++;
+		tmp_n = tmp_n->next;
+		l_n = head;
+		new_n = 0;
+		while (new_n < counter)
+		{
+			if (tmp_n == l_n)
+			{
+				printf("-> [%p] %d\n", (void *)tmp_n, tmp_n->n);
+				return (counter);
+			}
+			l_n = l_n->next;
+			new_n++;
+		}
+		if (!head)
+			exit(98);
+	}
+	return (counter);
+}
