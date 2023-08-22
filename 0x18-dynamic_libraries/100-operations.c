62e0b974db8c03c1c22a7bb08d96b45a60691806
devbn3li <fudwhc@gmail.com>
2023-08-22 03:43:38 +0300

    kkkk

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - sum
+ * @a: first num
+ * @b: sec num
+ * Return: result
+ */
+int add(int a, int b)
+{
+      return (a + b);
+}
+/**
+ * sub - add
+ * @a: first num
+ * @b: sec num
+ * Return: result
+ */
+int sub(int a, int b)
+{
+      return (a - b);
+}
+/**
+ * mul - addition
+ * @a: first num
+ * @b: sec num
+ * Return: result
+ */
+int mul(int a, int b)
+{
+      return (a * b);
+}
+/**
+ * div - add
+ * @a: first num
+ * @b: sec num
+ * Return: result
+ */
+int div(int a, int b)
+{
+      return (a / b);
+}
+/**
+ * mod - addition
+ * @a: 1st num
+ * @b: 2ed num
+ * Return: result
+ */
+int mod(int a, int b)
+{
+      return (a % b);
+}
