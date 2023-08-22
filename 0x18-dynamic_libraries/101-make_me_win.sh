a064fdd9bce9d5e1827226f58d40250a8b0834a8
devbn3li <fudwhc@gmail.com>
2023-08-22 04:02:50 +0300

    lib giga

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/devbn3li/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://github.com/devbn3li/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so"
