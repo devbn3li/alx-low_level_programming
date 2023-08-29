58a28399af2214cc7cf3ff6d6b7b4787d25c95f0
devbn3li <fudwhc@gmail.com>
2023-08-29 21:46:40 +0300

    ;

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,28 @@
 #include "main.h"
 /**
- * wildcmp - main
- * @s1: arg
- * @s2: arg
+ * wildcmp - Comp str
+ * @s1: ptr to str params
+ * @s2: ptr to str params
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
+		if (*s1 == '\0')
+		{
+			if (*s2 != '\0' && *s2 == '*')
+			{
+				return (wildcmp(s1, s2 + 1));
+			}
+			return (*s2 == '\0');
+		}
+
+		if (*s2 == '*')
+		{
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		}
+		else if (*s1 == *s2)
+		{
+			return (wildcmp(s1 + 1, s2 + 1));
+		}
+		return (0);
 }
