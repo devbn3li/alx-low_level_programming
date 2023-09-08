88fd25b4eb566aa52e11e59715a2978d45e57e06
devbn3li <fudwhc@gmail.com>
2023-09-09 01:55:55 +0300

    tt

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Print hash table
+ * @ht: Ptr to hash table to print
+ *
+ * Description: Key//value pairs are printed in
+ * the order they appear in the array
+ * 		of the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int x;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[x];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
